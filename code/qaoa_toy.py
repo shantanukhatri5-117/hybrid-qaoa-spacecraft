0fd90a24170eca34342365522ac74b7b7e4477d4
shantanukhatri5-117 <shantanukhatri5@gmail.com>
2025-12-09 20:10:37 +0530

    Add qaoa_toy.py minimal test

--- code/qaoa_toy.py
+++ code/qaoa_toy.py
@@ -6,32 +6,25 @@ from qiskit_optimization.algorithms import MinimumEigenOptimizer
 from qiskit.algorithms.minimum_eigensolvers import QAOA
 from qiskit.utils import algorithm_globals
 
-# Set seed
 algorithm_globals.random_seed = 42
 
-# Define toy graph (3 nodes)
 graph = {
     0: [1, 2],
     1: [0, 2],
     2: [0, 1]
 }
 
-# Build MAX-CUT problem
 maxcut = Maxcut(graph)
 qp = maxcut.to_quadratic_program()
 
-# Convert to QUBO
 qp2qubo = QuadraticProgramToQubo()
 qubo = qp2qubo.convert(qp)
 
-# Simulator backend
 backend = Aer.get_backend("aer_simulator_statevector")
 
-# QAOA with depth p=1
 qaoa = QAOAAnsatz(3, reps=1)
 optimizer = QAOA(qaoa=qaoa, optimizer=None)
 
-# Solve
 meo = MinimumEigenOptimizer(optimizer)
 result = meo.solve(qubo)
 
