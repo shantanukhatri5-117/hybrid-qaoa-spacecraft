7e337ce10d91b250ae3366f0ba7f41468257fa30
shantanukhatri5-117 <shantanukhatri5@gmail.com>
2025-12-09 21:37:59 +0530

    Update qaoa_toy.py

--- code/qaoa_toy.py
+++ code/qaoa_toy.py
@@ -1,31 +1,37 @@
-from qiskit import Aer, execute
-from qiskit.circuit.library import QAOAAnsatz
+from qiskit_aer import Aer
 from qiskit_optimization.applications.ising.max_cut import Maxcut
 from qiskit_optimization.converters import QuadraticProgramToQubo
 from qiskit_optimization.algorithms import MinimumEigenOptimizer
 from qiskit.algorithms.minimum_eigensolvers import QAOA
+from qiskit.circuit.library import QAOAAnsatz
 from qiskit.utils import algorithm_globals
 
+# Seed for reproducibility
 algorithm_globals.random_seed = 42
 
+# Define a simple 3-node graph
 graph = {
     0: [1, 2],
     1: [0, 2],
     2: [0, 1]
 }
 
+# Build MAX-CUT problem
 maxcut = Maxcut(graph)
 qp = maxcut.to_quadratic_program()
 
+# Convert to QUBO
 qp2qubo = QuadraticProgramToQubo()
 qubo = qp2qubo.convert(qp)
 
-backend = Aer.get_backend("aer_simulator_statevector")
+# Use Aer statevector backend
+backend = Aer.get_backend("statevector_simulator")
 
-qaoa = QAOAAnsatz(3, reps=1)
-optimizer = QAOA(qaoa=qaoa, optimizer=None)
+# QAOA ansatz
+qaoa = QAOA(ansatz=QAOAAnsatz(num_qubits=3, reps=1), optimizer=None, quantum_instance=backend)
 
-meo = MinimumEigenOptimizer(optimizer)
+# Solve
+meo = MinimumEigenOptimizer(qaoa)
 result = meo.solve(qubo)
 
 print("Optimal bitstring:", result.x)
